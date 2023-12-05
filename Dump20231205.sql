03f06ddfaf170f3e04361acf76b96803e7f7943c
Brian-boudrioux <135231975+Brian-boudrioux@users.noreply.github.com>
2023-12-05 16:25:54 +0100

    Update Dump20231205.sql

--- Dump20231205.sql
+++ Dump20231205.sql
@@ -19,6 +19,9 @@
 -- Table structure for table `albums`
 --
 
+CREATE database testdb;
+use testdb;
+
 DROP TABLE IF EXISTS `albums`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
